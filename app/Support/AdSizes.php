f70257db1b7390583f177a15b15965040194b6fa
meenakshi1811 <136489830+meenakshi1811@users.noreply.github.com>
2026-04-24 12:13:19 +0530

    Add homepage-specific ad size options for admin templates

--- app/Support/AdSizes.php
+++ app/Support/AdSizes.php
@@ -16,6 +16,11 @@ final class AdSizes
             'square_large' => ['name' => 'Square Large', 'ratio' => '1 / 1', 'w' => 900, 'h' => 900],
             'banner' => ['name' => 'Banner', 'ratio' => '4 / 1', 'w' => 1200, 'h' => 300],
             'full_page' => ['name' => 'Full page', 'ratio' => '3 / 4', 'w' => 900, 'h' => 1200],
+            'home_top_slider' => ['name' => 'Home Top Slider', 'ratio' => '4 / 1', 'w' => 1200, 'h' => 300],
+            'home_side_slider' => ['name' => 'Home Side Slider', 'ratio' => '5 / 7', 'w' => 500, 'h' => 700],
+            'home_recent_card' => ['name' => 'Home Recent Card', 'ratio' => '4 / 3', 'w' => 800, 'h' => 600],
+            'home_offer_poster' => ['name' => 'Home Offer Poster', 'ratio' => '64 / 90', 'w' => 768, 'h' => 1080],
+            'home_wide_slider' => ['name' => 'Home Wide Slider', 'ratio' => '16 / 5', 'w' => 1600, 'h' => 500],
         ];
     }
 
@@ -29,4 +34,3 @@ final class AdSizes
         return self::all()[$sizeType]['name'] ?? $sizeType;
     }
 }
-
