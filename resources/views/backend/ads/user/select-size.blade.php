9001a561201ed7914d87b16ea5405b18060f8bb5
meenakshi1811 <136489830+meenakshi1811@users.noreply.github.com>
2026-05-05 16:47:00 +0530

    Show ad size previews at true pixel widths

--- resources/views/backend/ads/user/select-size.blade.php
+++ resources/views/backend/ads/user/select-size.blade.php
@@ -12,10 +12,6 @@
         </div>
     </div>
 
-    @php
-        $maxWidth = max(array_column($sizes, 'w'));
-    @endphp
-
     <div class="chart-card">
         <div class="row g-3">
             @foreach($sizes as $sizeType => $size)
@@ -27,10 +23,7 @@
                                 <span class="badge text-bg-warning">Admin Placement</span>
                             @endif
                         </div>
-                        @php
-                            $previewScale = 0.7 + (0.3 * ($size['w'] / $maxWidth));
-                        @endphp
-                        <div class="ads-size-shape" style="aspect-ratio: {{ $size['ratio'] }}; width: min(100%, {{ round($previewScale * 100, 2) }}%); margin-inline: auto;">
+                        <div class="ads-size-shape" style="aspect-ratio: {{ $size['ratio'] }}; width: min(100%, {{ $size['w'] }}px); margin-inline: auto;">
                             <div class="ads-size-shape-inner">
                                 <span class="ads-size-dim">{{ $size['w'] }}×{{ $size['h'] }}</span>
                             </div>
