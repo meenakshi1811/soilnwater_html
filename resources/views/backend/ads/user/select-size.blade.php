23cfb761704658430a49c7d0d2cc0b9f247a21ba
meenakshi1811 <136489830+meenakshi1811@users.noreply.github.com>
2026-05-05 16:51:26 +0530

    Make ad preview widths proportional to actual sizes

--- resources/views/backend/ads/user/select-size.blade.php
+++ resources/views/backend/ads/user/select-size.blade.php
@@ -28,9 +28,9 @@
                             @endif
                         </div>
                         @php
-                            $previewScale = 0.7 + (0.3 * ($size['w'] / $maxWidth));
+                            $previewWidthPercent = ($size['w'] / $maxWidth) * 100;
                         @endphp
-                        <div class="ads-size-shape" style="aspect-ratio: {{ $size['ratio'] }}; width: min(100%, {{ round($previewScale * 100, 2) }}%); margin-inline: auto;">
+                        <div class="ads-size-shape" style="aspect-ratio: {{ $size['ratio'] }}; width: min(100%, {{ round($previewWidthPercent, 2) }}%); margin-inline: auto;">
                             <div class="ads-size-shape-inner">
                                 <span class="ads-size-dim">{{ $size['w'] }}×{{ $size['h'] }}</span>
                             </div>
