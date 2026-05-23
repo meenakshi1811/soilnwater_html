40966764b274bef253665a12329ab6f96a42bd6a
Meenakshi Nanta <nanta1811@gmail.com>
2026-05-23 11:39:23 +0000

    vendor changes

--- resources/views/frontend/store/contact.blade.php
+++ resources/views/frontend/store/contact.blade.php
@@ -81,6 +81,10 @@
 @endpush
 
 @push('store_scripts')
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 @if($inquiryProduct)
 <script>
 document.getElementById('enquiryForm')?.addEventListener('submit', async function(e){
--- resources/views/frontend/store/product-show.blade.php
+++ resources/views/frontend/store/product-show.blade.php
@@ -152,6 +152,9 @@ $adFrameStyle = function ($ad) {
 @endpush
 @push('store_scripts')
 @auth
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 <script>
 document.getElementById('enquiryForm')?.addEventListener('submit', async function(e){
     e.preventDefault();
