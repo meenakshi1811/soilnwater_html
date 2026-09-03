16dc73bfcdf630b305c1aedcd7b6fc4f2eed1156
meenakshi1811 <136489830+meenakshi1811@users.noreply.github.com>
2026-09-03 14:35:20 +0530

    new

--- resources/views/frontend/vendors/partials/premium-cards.blade.php
+++ resources/views/frontend/vendors/partials/premium-cards.blade.php
@@ -21,9 +21,14 @@
         </div>
         <div class="vendors-premium-card__body">
             <h3 class="vendors-premium-card__name">{{ $vendor->publicDisplayName() }}</h3>
+            <span class="vendors-premium-card__category">{{ $categoryName }}</span>
             <div class="vendors-premium-card__meta">
                 <span><i class="fa-solid fa-location-dot" aria-hidden="true"></i>{{ $locationLabel }}</span>
             </div>
+            <p class="vendors-premium-card__product-heading">
+                <span class="vendors-premium-card__product-label">Deals in:</span>
+                {{ $featuredLabel ?: $categoryName }}
+            </p>
             <div class="vendors-premium-card__rating-row">
                 <span class="vendors-rating">
                     <i class="fa-solid fa-star" aria-hidden="true"></i>{{ number_format($ratingScore, 1) }}
