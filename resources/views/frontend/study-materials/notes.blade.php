cb3f363764a9eaf3d393ad95ddacf82c898b9f5f
meenakshi1811 <136489830+meenakshi1811@users.noreply.github.com>
2026-09-07 22:46:57 +0530

    new

--- resources/views/frontend/study-materials/notes.blade.php
+++ resources/views/frontend/study-materials/notes.blade.php
@@ -237,6 +237,33 @@
                     @endforeach
                 </div>
 
+                <p class="sm-results-count">
+                    Showing {{ $materials->firstItem() ?: 0 }} to {{ $materials->lastItem() ?: 0 }} of {{ number_format($materials->total()) }} notes
+                </p>
+
+                @if($viewMode === 'grid')
+                    <div class="sm-grid-cards">
+                        @forelse($materials as $item)
+                            @include('frontend.study-materials.partials.note-grid-item', ['item' => $item])
+                        @empty
+                            <p class="sm-empty">No notes found for the selected filters.</p>
+                        @endforelse
+                    </div>
+                @else
+                    <div class="sm-note-list">
+                        @forelse($materials as $item)
+                            @include('frontend.study-materials.partials.note-list-item', ['item' => $item])
+                        @empty
+                            <p class="sm-empty">No notes found for the selected filters.</p>
+                        @endforelse
+                    </div>
+                @endif
+
+                @if($materials->hasPages())
+                    <div class="sm-pagination-wrap">
+                        {{ $materials->onEachSide(1)->links('frontend.study-materials.partials.pagination') }}
+                    </div>
+                @endif
             </section>
 
             <aside class="sm-right-sidebar">
@@ -300,36 +327,6 @@
                     </div>
                 </div>
             </aside>
-        </div>
-
-        <div class="sm-notes-results">
-            <p class="sm-results-count">
-                Showing {{ $materials->firstItem() ?: 0 }} to {{ $materials->lastItem() ?: 0 }} of {{ number_format($materials->total()) }} notes
-            </p>
-
-            @if($viewMode === 'grid')
-                <div class="sm-grid-cards">
-                    @forelse($materials as $item)
-                        @include('frontend.study-materials.partials.note-grid-item', ['item' => $item])
-                    @empty
-                        <p class="sm-empty">No notes found for the selected filters.</p>
-                    @endforelse
-                </div>
-            @else
-                <div class="sm-note-list">
-                    @forelse($materials as $item)
-                        @include('frontend.study-materials.partials.note-list-item', ['item' => $item])
-                    @empty
-                        <p class="sm-empty">No notes found for the selected filters.</p>
-                    @endforelse
-                </div>
-            @endif
-
-            @if($materials->hasPages())
-                <div class="sm-pagination-wrap">
-                    {{ $materials->onEachSide(1)->links('frontend.study-materials.partials.pagination') }}
-                </div>
-            @endif
         </div>
     </div>
 </div>
